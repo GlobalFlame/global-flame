14cd44e5362b2dd0508e25be0c7fe409ad1f342a
Baby Boy Lightning <founder@globalflame.org>
2025-06-08 20:45:01 -0400

    fix: app/layout.tsx syntax regression

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
-﻿/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
-}
-module.exports = nextConfig
+  allowedDevOrigins: ['http://localhost:3000', 'http://192.168.1.147:3000'],
+  webpack: (config) => {
+    config.cache = {
+      ...config.cache,
+      type: 'filesystem',
+      cacheDirectory: path.resolve(__dirname, '.next/cache/webpack'),
+      maxAge: 60000 // 60 seconds
+    };
+    return config;
+  }
+};
+
+module.exports = nextConfig;
