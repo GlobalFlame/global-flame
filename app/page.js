4d16d3d2d96ae29d55ed237404e8244aaa0efc74
Baby Boy Lightning <founder@globalflame.org>
2025-06-25 19:58:03 -0400

    🔥 Clean homepage & fix broken modules for Twilio verification

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 ﻿export default function Home() {
   return (
     <div style={{ padding: "3rem", fontFamily: "sans-serif" }}>
-      <h1>🔥 Welcome to Global Flame</h1>
+      <h1>{'🔥 Welcome to Global Flame'}</h1>
       <p>Operated by LB11 LLC</p>
       <p>Email: <a href="mailto:founder@globalflame.org">founder@globalflame.org</a></p>
       <p>Trademark Serial: 99136991</p>
