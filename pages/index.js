d2f8c4686a4ae5946dfcb09951b38815fab7f738
Baby Boy Lightning <founder@globalflame.org>
2025-06-25 19:29:44 -0400

    🔥 Add homepage for Twilio verification

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 ﻿export default function Home() {
   return (
-    <div style={{ padding: '3rem', fontFamily: 'sans-serif' }}>
+    <div style={{ padding: "3rem", fontFamily: "sans-serif" }}>
       <h1>🔥 Welcome to Global Flame</h1>
       <p>Operated by LB11 LLC</p>
       <p>Email: <a href="mailto:founder@globalflame.org">founder@globalflame.org</a></p>
